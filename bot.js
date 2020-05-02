98ba2dfdd6ae049aee4ac842b83b22fd016bb065
VEKKX <44268685+EXERIOS-GAMER2007@users.noreply.github.com>
2020-05-02 15:53:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,8 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "443121178253590528") return;
+  if (message.author.id !== "585052462914863104
+     ") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
